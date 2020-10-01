5312cae9c214632b130b9d6d39bd7a1e4aa01b52
Iyeritufu Precious <iyeritufu@gmail.com>
2020-10-01 02:52:20 +0100

    getting ready for deployment

---
+++ server.js
@@ -0,0 +1,15 @@
+const { Console } = require("console");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + "/dist/openmarket-seller"));
+
+app.listen(process.env.PORT || 8080);
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/openmarket-seller/index.html"));
+});
+
+console.log("apps is running");
