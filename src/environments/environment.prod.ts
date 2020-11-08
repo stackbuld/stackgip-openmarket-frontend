317ec366139ff8af42ef2d55a20ec05c35971491
Iyeritufu Precious <iyeritufu@gmail.com>
2020-11-08 17:40:26 +0100

    updated add-product

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,6 @@ export const environment = {
   identityApi: "https://openmarketauth.herokuapp.com/api/v1/",
   ecommerceApi: "https://openmarket-ecommerce.herokuapp.com/api/v1/",
   paystackPublicKey: "pk_test_8e75de0f0e2bba17ee0b37ed227e176b92b52d0b",
+  cloudinaryName: "dimxqjzpr",
+  cloudinaryUploadPerset: "unsigned1_openmarket",
 };
