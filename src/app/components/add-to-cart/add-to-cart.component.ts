0f09da95c23762d4af9aaae9d888b44da916aed5
precious iyeritufu <iyeritufu@gmail.com>
2022-02-06 22:17:07 +0100

    update checkout

--- src/app/components/add-to-cart/add-to-cart.component.ts
+++ src/app/components/add-to-cart/add-to-cart.component.ts
@@ -12,142 +12,157 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: "./add-to-cart.component.html",
   styleUrls: ["./add-to-cart.component.css"],
 })
-export class AddToCartComponent implements OnInit{
-  @Input() productId: number
-  @Output() addedToCart = new EventEmitter()
-  @Output() closed = new EventEmitter()
-  productOptions:{[title:string]:CreateProductOption[]}
-  selectedProductOptions:{[title:string]:string} = {}
-  product: ProductWithOptionAndShipmentModel = null
-  currentOptions:{[title:string]:number} = {}
-  numberWithCommas:Function
-  priceWithOptions:number
-  currentQuantity:number
-  optionForm: FormGroup
-  formatDate:Function
-  totalPrice:number
+export class AddToCartComponent implements OnInit {
+  @Input() productId: number;
+  @Output() addedToCart = new EventEmitter();
+  @Output() closed = new EventEmitter();
+  productOptions: { [title: string]: CreateProductOption[] };
+  selectedProductOptions: { [title: string]: string } = {};
+  product: ProductWithOptionAndShipmentModel = null;
+  currentOptions: { [title: string]: number } = {};
+  numberWithCommas: Function;
+  priceWithOptions: number;
+  currentQuantity: number;
+  optionForm: FormGroup;
+  formatDate: Function;
+  totalPrice: number;
   availablePaymentOption = [];
+
   constructor(
-    private productService: ProductsService,private toast: ToastrService,
-    private fb: FormBuilder) {
-    this.numberWithCommas = numberWithCommas
+    private productService: ProductsService,
+    private toast: ToastrService,
+    private fb: FormBuilder
+  ) {
+    this.numberWithCommas = numberWithCommas;
     this.formatDate = formatDate;
   }
 
   ngOnInit(): void {
-    this.productService.getProductById(this.productId.toString()).subscribe((a) => {
-        this.product = (a.data as unknown) as ProductWithOptionAndShipmentModel  
-        this.init() 
-    });
+    this.productService
+      .getProductById(this.productId.toString())
+      .subscribe((a) => {
+        this.product = a.data as unknown as ProductWithOptionAndShipmentModel;
+        this.init();
+      });
     this.optionForm = this.fb.group({
-      shipmentOption:['',[Validators.required]],
-      quantity:[1,[Validators.required]],
+      shipmentOption: ["", [Validators.required]],
+      quantity: [1, [Validators.required]],
       options: this.fb.array([]),
-      paymentOption:['',[Validators.required]]
+      paymentOption: ["", [Validators.required]],
     });
-    this.optionForm.get('quantity').valueChanges.pipe(debounceTime(200))
-    .subscribe(data => this.onQuantityValueChanged(data))
+    this.optionForm
+      .get("quantity")
+      .valueChanges.pipe(debounceTime(200))
+      .subscribe((data) => this.onQuantityValueChanged(data));
     this.currentQuantity = 1;
-
   }
 
   init(): void {
-    this.productOptions = this.groupBy('title')
-    this.totalPrice = this.product.price
-    this.priceWithOptions = this.product.price
-    const defaultShipment:string = this.getDefaultShipment()
-    this.optionForm.get('shipmentOption').setValue(defaultShipment)
+    this.productOptions = this.groupBy("title");
+    this.totalPrice = this.product.price;
+    this.priceWithOptions = this.product.price;
+    const defaultShipment: string = this.getDefaultShipment();
+    this.optionForm.get("shipmentOption").setValue(defaultShipment);
     this.setOptionForm(Object.keys(this.productOptions));
-    this.availablePaymentOption = this.product.paymentOptions.split(',')
+
+    this.availablePaymentOption = this.product.paymentOptions
+      ? this.product.paymentOptions.split(",")
+      : [];
+    console.log("payment options", this.availablePaymentOption);
   }
 
-  optionArray():FormArray{
-    return this.optionForm.get('options') as FormArray;
+  optionArray(): FormArray {
+    return this.optionForm.get("options") as FormArray;
   }
 
-  setOptionForm(options:string[]):void{
+  setOptionForm(options: string[]): void {
     options.forEach((option) => {
-      let {title, value, cost} = this.productOptions[option][0]
+      let { title, value, cost } = this.productOptions[option][0];
       let itemToAdd = this.fb.group({
-        title:option,cost:[`${cost}|${value}`,[]]
-      })
-      itemToAdd.valueChanges.pipe(debounceTime(200)).subscribe(
-        data => this.onOptionValueChanged(data)
-      )
-      this.optionArray().push(itemToAdd)
-      this.currentOptions[option] = 0
-      cost = String(cost)
-      this.onOptionValueChanged({title,cost})
+        title: option,
+        cost: [`${cost}|${value}`, []],
+      });
+      itemToAdd.valueChanges
+        .pipe(debounceTime(200))
+        .subscribe((data) => this.onOptionValueChanged(data));
+      this.optionArray().push(itemToAdd);
+      this.currentOptions[option] = 0;
+      cost = String(cost);
+      this.onOptionValueChanged({ title, cost });
     });
   }
 
-  onQuantityValueChanged(qty:number){
-    this.currentQuantity = qty
-    this.totalPrice = this.priceWithOptions * this.currentQuantity
+  onQuantityValueChanged(qty: number) {
+    this.currentQuantity = qty;
+    this.totalPrice = this.priceWithOptions * this.currentQuantity;
   }
 
-  onOptionValueChanged(data:{title:string,cost:string}){
-    const [cost,value] = data.cost.split('|')
-    const numCost = Number(cost)
-    this.priceWithOptions -= this.currentOptions[data.title]
-    this.priceWithOptions += numCost
-    this.currentOptions[data.title] = numCost
-    this.totalPrice = Number(this.priceWithOptions) * Number(this.currentQuantity)
-    this.selectedProductOptions[data.title] = value 
+  onOptionValueChanged(data: { title: string; cost: string }) {
+    const [cost, value] = data.cost.split("|");
+    const numCost = Number(cost);
+    this.priceWithOptions -= this.currentOptions[data.title];
+    this.priceWithOptions += numCost;
+    this.currentOptions[data.title] = numCost;
+    this.totalPrice =
+      Number(this.priceWithOptions) * Number(this.currentQuantity);
+    this.selectedProductOptions[data.title] = value;
   }
 
-  addToCart(){
-    const paymentOption = this.optionForm.get('paymentOption')
-    const shipmentOption = this.optionForm.get('shipmentOption')
-    let currentShipmentOption:string = 'state|city|0'
-    if(this.product.productShipments.length > 0){
-      currentShipmentOption = shipmentOption.value
+  addToCart() {
+    const paymentOption = this.optionForm.get("paymentOption");
+    const shipmentOption = this.optionForm.get("shipmentOption");
+    let currentShipmentOption: string = "state|city|0";
+    if (this.product.productShipments.length > 0) {
+      currentShipmentOption = shipmentOption.value;
     }
-    if(this.optionForm.get('quantity').value < 1){
-      this.toast.error("please enter a valid quantity")
-      return
+    if (this.optionForm.get("quantity").value < 1) {
+      this.toast.error("please enter a valid quantity");
+      return;
     }
-    const [state, city, cost] = currentShipmentOption.split('|')
+    const [state, city, cost] = currentShipmentOption.split("|");
     this.moveProductToCart({
-      shipmentOption:JSON.stringify({state,city,cost}),
-      paymentOption:paymentOption.value,
-      productOptions:JSON.stringify(this.selectedProductOptions)
-    })
+      shipmentOption: JSON.stringify({ state, city, cost }),
+      paymentOption: paymentOption.value,
+      productOptions: JSON.stringify(this.selectedProductOptions),
+    });
   }
 
-  moveProductToCart(cartOption){
-    this.product.price = this.priceWithOptions
-    this.addedToCart.emit(
-      {
-        product:this.product,
-        orderedUnit:this.currentQuantity,
-        paymentOption:cartOption.paymentOption,
-        shipmentOption:cartOption.shipmentOption,
-        productOptions:cartOption.productOptions
-      }
-    )
+  moveProductToCart(cartOption) {
+    this.product.price = this.priceWithOptions;
+    this.addedToCart.emit({
+      product: this.product,
+      orderedUnit: this.currentQuantity,
+      paymentOption: cartOption.paymentOption,
+      shipmentOption: cartOption.shipmentOption,
+      productOptions: cartOption.productOptions,
+    });
   }
 
-  groupBy(property:string):{[title:string]:CreateProductOption[]}{
+  groupBy(property: string): { [title: string]: CreateProductOption[] } {
     return this.product.productOptions.reduce((acc, obj) => {
-       const key = obj[property]
-       if (!acc[key]) {acc[key] = []}
-       acc[key].push(obj)
-       return acc
-    }, {})
+      const key = obj[property];
+      if (!acc[key]) {
+        acc[key] = [];
+      }
+      acc[key].push(obj);
+      return acc;
+    }, {});
   }
 
-  close(){
-    this.closed.emit()
+  close() {
+    this.closed.emit();
   }
 
-  getDefaultShipment(){
-    let shipmentOptionDefault:string = ''
-    if(this.product.productShipments.length > 0){
-        shipmentOptionDefault = this.product.productShipments[0].state+'|'+
-        this.product.productShipments[0].city+'|'+
-        this.product.productShipments[0].cost
+  getDefaultShipment() {
+    let shipmentOptionDefault: string = "";
+    if (this.product.productShipments.length > 0) {
+      shipmentOptionDefault =
+        this.product.productShipments[0].state +
+        "|" +
+        this.product.productShipments[0].city +
+        "|" +
+        this.product.productShipments[0].cost;
     }
-    return shipmentOptionDefault
+    return shipmentOptionDefault;
   }
 }
