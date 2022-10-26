74a22df3fa882f45d1405e36d0f62d6831d4d618
David Egbuwalo <lereegbuwalo@gmail.com>
2022-10-26 14:57:01 +0100

    update on seller registration form

--- src/app/models/sellerModel.ts
+++ src/app/models/sellerModel.ts
@@ -1,19 +1,23 @@
 import { ResponseModel } from "../shared/models/ResponseModel";
 
 export interface ISeller {
-    businessName: 'string';
-    businessAddress: 'string';
-    businessEmail: 'string';
-    businessPhone: 'string';
-    businessCountryCode: 'string';
-    businessState: 'string';
-    businessCity: 'string';
-    businessLogoUrl: 'string' | string;
-    businessRegistrationNumber: 'string';
-    address:'string';
-    phoneNumber:'string';
+  businessName: "string";
+  businessAddress: "string";
+  businessEmail: "string";
+  businessPhone: "string";
+  businessCountryCode: "string";
+  businessState: "string";
+  businessCity: "string";
+  businessLogoUrl?: "string" | string;
+  businessRegistrationNumber?: "string";
+  address: "string";
+  phoneNumber: "string";
+  businessApplicantAddress?: string;
+  businessAddressLandmark?: string;
+  businessApplicantID: string;
+  businessIDType: string;
 }
 
 export interface GetSeller extends ResponseModel {
-    data: ISeller;
-  }
+  data: ISeller;
+}
