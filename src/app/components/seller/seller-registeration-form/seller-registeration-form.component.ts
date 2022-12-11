3f4029c33908e74a5f6bfe0f0de0e21d7d5fda25
Victoria Abioye <victoriousvikky003@gmail.com>
2022-12-11 14:53:45 +0100

    new payload is implemented

--- src/app/components/seller/seller-registeration-form/seller-registeration-form.component.ts
+++ src/app/components/seller/seller-registeration-form/seller-registeration-form.component.ts
@@ -7,7 +7,8 @@ import {
   OnInit,
   Output,
 } from "@angular/core";
-import { FormBuilder, Validators } from "@angular/forms";
+// import { FormBuilder, Validators } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import uikit from "uikit";
 import { Subscription } from "rxjs";
 import { IUser } from "src/app/models/IUserModel";
@@ -34,27 +35,30 @@ export class SellerRegisterationFormComponent
   imageName: string;
   isLoading = false;
   uploadWidget: any;
-  componentForm = this.fb.group({
-    registerationStatus: ["registered"],
-    businessName: ["", [Validators.required]],
-    businessPhone: ["", [Validators.required]],
-    businessEmail: ["", [Validators.required, Validators.email]],
-    businessAddress: ["", [Validators.required]],
-    businessCountry: ["", [Validators.required]],
-    businessState: ["", [Validators.required]],
-    businessCity: ["", [Validators.required]],
-    businessLogoUrl: [
-      "",
-      [
+  sellerRegFormGroup: FormGroup;
+  applicant: FormGroup;
+  idCardTypes = ["NIN", "Voter's Card", "International Passport", "Driver's Licence"];
+  // componentForm = this.fb.group({
+    // registerationStatus: ["registered"],
+    // businessName: ["", [Validators.required]],
+    // businessPhone: ["", [Validators.required]],
+    // businessEmail: ["", [Validators.required, Validators.email]],
+    // businessAddress: ["", [Validators.required]],
+    // businessCountry: ["", [Validators.required]],
+    // businessState: ["", [Validators.required]],
+    // businessCity: ["", [Validators.required]],
+    // businessLogoUrl: [
+    //   "",
+    //   [
         // Validators.required,
-      ],
-    ],
-    businessRegistrationNumber: [""],
-    businessApplicantAddress: ["", [Validators.required]],
-    businessAddressLandmark: ["", [Validators.required]],
-    businessApplicantID: ["", [Validators.required]],
-    businessIDType: ["", [Validators.required]],
-  });
+    //   ],
+    // ],
+    // businessRegistrationNumber: [""],
+    // businessApplicantAddress: ["", [Validators.required]],
+    // businessAddressLandmark: ["", [Validators.required]],
+    // businessApplicantID: ["", [Validators.required]],
+    // businessIDType: ["", [Validators.required]],
+  // });
   regSeller$: Subscription;
 
   states = nigeriaSates.map((a) => a.name.toLowerCase());
@@ -65,7 +69,8 @@ export class SellerRegisterationFormComponent
   ) {}
 
   ngOnInit(): void {
-    this.setBusinessCategoryValidators ()
+    this.sellerRegForm();
+    this.setBusinessCategoryValidators();
     // open modal
     uikit.modal("#seller-modal-full").show();
     this.uploadWidget = cloudinary.createUploadWidget(
@@ -83,25 +88,47 @@ export class SellerRegisterationFormComponent
       }
     );
 
-    console.log(this.componentForm);
+    console.log("Formcontrol",this.sellerRegFormGroup);
+  }
+
+  sellerRegForm(): void {
+    this.applicant = this.fb.group({
+      idType: ['', Validators.required],
+      idNumber: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+    })
+    this.sellerRegFormGroup = this.fb.group({
+  userId: ['', Validators.required],
+  businessName: ['', Validators.required],
+  businessDescription: ['', Validators.required],
+  businessAddress: ['', Validators.required],
+  businessLogo: [''],
+  businessRegistrationNumber: ['', Validators.required],
+  street: ['', Validators.required],
+  lga: ['', Validators.required],
+  isBusinessRegistered: [true, Validators.required],
+  state: ['', Validators.required],
+  landmark: ['', Validators.required],
+  applicant: this.applicant
+    })
   }
 
   ngAfterViewChecked(): void {
-    if (this.componentForm.get("registerationStatus").value == "registered") {
-      this.componentForm.get("businessApplicantAddress").disable();
-      this.componentForm.get("businessAddressLandmark").disable();
-    } else {
-      this.componentForm.get("businessApplicantAddress").enable();
-      this.componentForm.get("businessAddressLandmark").enable();
+    if (this.sellerRegFormGroup.get("isBusinessRegistered").value == "true") {
+      this.sellerRegFormGroup.get("businessRegistrationNumber").enable();
     }
 
     if (this.imageName) {
-      this.componentForm.get("businessLogoUrl").setValue(this.imageName);
+      this.sellerRegFormGroup.get("businessLogo").setValue(this.imageName);
     }
   }
 
-  get registerationStatus() {
-    return this.componentForm.get("registerationStatus").value;
+  // get registerationStatus() {
+  //   return this.componentForm.get("registerationStatus").value;
+  // }
+
+  get isBusinessRegistered() {
+    return this.sellerRegFormGroup.get("isBusinessRegistered").value;
   }
 
   submit() {
@@ -127,13 +154,13 @@ export class SellerRegisterationFormComponent
     //   }
     // );
 
-    if (this.componentForm.invalid) {
+    if (this.sellerRegFormGroup.invalid) {
       this.toast.error("Please fill fields appropriately");
     } else if (!this.image) {
       this.toast.error("business logo is required");
     } else {
-      const sellerData: ISeller = this.componentForm.value;
-      sellerData.businessLogoUrl = this.image;
+      const sellerData: ISeller = this.sellerRegFormGroup.value;
+      sellerData.businessLogo = this.image;
       this.isLoading = true;
       this.regSeller$ = this.sellerS.registerSeller(sellerData).subscribe(
         (res: { user: IUser; response: ResponseModel }) => {
@@ -169,27 +196,35 @@ export class SellerRegisterationFormComponent
     }
   }
 
-  setBusinessCategoryValidators (){
-    const businessRegNumberControl = this.componentForm.get('businessRegistrationNumber');
-    const businessApplicantAddressControl = this.componentForm.get('businessApplicantAddress');
-    const businessAddressLandmarkControl = this.componentForm.get('businessAddressLandmark');
+  setBusinessCategoryValidators() {
+    const businessRegNumberControl = this.sellerRegFormGroup.get(
+      "businessRegistrationNumber"
+    );
+    // const businessApplicantAddressControl = this.componentForm.get(
+    //   "businessApplicantAddress"
+    // );
+    // const businessAddressLandmarkControl = this.componentForm.get(
+    //   "businessAddressLandmark"
+    // );
 
-    this.componentForm.get('registerationStatus').valueChanges.subscribe(registerationStatus => {
-      if (registerationStatus === 'registered') {
-        businessRegNumberControl.setValidators([Validators.required]);
-        businessApplicantAddressControl.setValidators(null);
-        businessAddressLandmarkControl.setValidators(null);
-      }
+    this.sellerRegFormGroup
+      .get("isBusinessRegistered")
+      .valueChanges.subscribe((isBusinessRegistered) => {
+        if (isBusinessRegistered === "true") {
+          businessRegNumberControl.setValidators([Validators.required]);
+          // businessApplicantAddressControl.setValidators(null);
+          // businessAddressLandmarkControl.setValidators(null);
+        }
 
-      if (registerationStatus === 'unregistered') {
-        businessRegNumberControl.setValidators(null);
-        businessApplicantAddressControl.setValidators([Validators.required]);
-        businessAddressLandmarkControl.setValidators([Validators.required]);
-      }
+        if (isBusinessRegistered === "false") {
+          businessRegNumberControl.setValidators(null);
+          // businessApplicantAddressControl.setValidators([Validators.required]);
+          // businessAddressLandmarkControl.setValidators([Validators.required]);
+        }
 
-      businessRegNumberControl.updateValueAndValidity();
-      businessApplicantAddressControl.updateValueAndValidity();
-      businessAddressLandmarkControl.updateValueAndValidity();
-    })
+        businessRegNumberControl.updateValueAndValidity();
+        // businessApplicantAddressControl.updateValueAndValidity();
+        // businessAddressLandmarkControl.updateValueAndValidity();
+      });
   }
 }
