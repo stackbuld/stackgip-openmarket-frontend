62e9bf4ecfc91d396c0ce85304087a9a40613a8b
kehinde onyekuwuluje <mac@macs-MacBook-Pro-2.local>
2023-01-30 20:36:37 +0100

    working on auth-login

--- src/app/components/seller/sidebar/sidebar.component.ts
+++ src/app/components/seller/sidebar/sidebar.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { IUser } from "src/app/models/IUserModel";
 import { SellerService } from 'src/app/services/seller/seller.service';
@@ -8,17 +9,25 @@ import { SellerService } from 'src/app/services/seller/seller.service';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SellerSidebarComponent implements OnInit {
-  logout() {
-    throw new Error('Method not implemented.');
-  }
   loggedInUser: IUser;
 
   constructor(
+    private router: Router,
     private sellerS: SellerService
   ) { }
 
   ngOnInit(): void {
     this.loggedInUser = this.sellerS.loggedInUser;
+  }
+
+  cancel = () => {
+    document.getElementById('closeLogoutModalBtn').click();
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+    this.cancel();
   }
 
 }
