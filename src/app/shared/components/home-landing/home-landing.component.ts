c7b73334edfb456430dcbeb8e84239a72cddb564
Victoria Abioye <victoria.abioye@Terra-Admins-MacBook-Pro-7.local>
2023-02-05 04:12:00 +0100

    social media link is added on desktop view, why join font-size, distorted page are fixed

--- src/app/shared/components/home-landing/home-landing.component.ts
+++ src/app/shared/components/home-landing/home-landing.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
@@ -6,8 +8,17 @@ import { NavbarComponent } from "../navbar/navbar.component";
   templateUrl: "./home-landing.component.html",
   styleUrls: ["./home-landing.component.css"],
 })
-export class HomeLandingComponent implements OnInit {
-  constructor() {}
+export class HomeLandingComponent implements AfterViewInit {
+  constructor(private http: HttpClient, private activeRoute: ActivatedRoute,) {}
+  @ViewChild('container') container: ElementRef<HTMLElement>;
 
-  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+
+    this.activeRoute.params.subscribe(param => {
+      if(param.pageSec){
+        const section = this.container.nativeElement.querySelector(`#${param.pageSec}`)
+        section?.scrollIntoView()
+      }
+    })
+  }
 }
