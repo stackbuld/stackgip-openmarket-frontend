ff29bc3698cab98de480d11297bb1adc46a9c76f
David Egbuwalo <lereegbuwalo@gmail.com>
2023-10-27 20:01:17 +0100

    update

--- src/app/modules/home/product-add-to-cart/product-add-to-cart.component.ts
+++ src/app/modules/home/product-add-to-cart/product-add-to-cart.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FooterService } from 'src/app/services/footer.service';
 
 @Component({
   selector: 'app-product-add-to-cart',
   templateUrl: './product-add-to-cart.component.html',
-  styleUrls: ['./product-add-to-cart.component.scss']
+  styleUrls: ['./product-add-to-cart.component.scss'],
 })
-export class ProductAddToCartComponent {
+export class ProductAddToCartComponent implements OnInit, OnDestroy {
+  constructor(private footerService: FooterService) {}
 
+  ngOnInit(): void {
+    this.footerService.setShowFooter(true);
+  }
+  ngOnDestroy(): void {}
 }
