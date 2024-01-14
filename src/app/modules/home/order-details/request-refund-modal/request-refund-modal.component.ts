0e943de9bc295ab26e8137df228bdfc44ed3e672
David Egbuwalo <lereegbuwalo@gmail.com>
2024-01-14 19:02:17 +0100

    update

--- src/app/modules/home/order-details/request-refund-modal/request-refund-modal.component.ts
+++ src/app/modules/home/order-details/request-refund-modal/request-refund-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { environment } from 'src/environments/environment';
+declare var cloudinary: any;
 
 @Component({
   selector: 'app-request-refund-modal',
@@ -14,10 +16,30 @@ export class RequestRefundModalComponent implements OnInit {
     'Item received broken or damaged',
     'Item seem to be fake and unauthentic',
   ];
+  uploadWidget: any;
+  mediaUrl: string;
 
   constructor(private dialog: MatDialog) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.uploadWidget = cloudinary.createUploadWidget(
+      {
+        cloudName: environment.cloudinaryName,
+        uploadPreset: environment.cloudinaryUploadPerset,
+        clientAllowedFormats: ['jpeg', 'jpg', 'png', 'gif', 'mp4'],
+      },
+      (error, result) => {
+        if (!error && result && result.event === 'success') {
+          this.mediaUrl = result.info.secure_url;
+          console.log(this.mediaUrl);
+        }
+      }
+    );
+  }
+
+  onUploadMedia() {
+    this.uploadWidget.open();
+  }
 
   onCloseModal() {
     this.dialog.closeAll();
