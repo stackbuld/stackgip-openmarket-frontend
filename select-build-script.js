daa9bca13b488fa174579c02a07120099ee42b85
Precious Iyeritufu <iyeritufu@gmail.com>
2024-01-16 09:12:52 +0100

    update

---
+++ select-build-script.js
@@ -0,0 +1,28 @@
+const exec = require('child_process').exec;
+
+const env = process.env.BUILD_ENV;
+
+let command;
+
+switch (env) {
+  case 'production':
+    command = 'yarn build:ssr';
+    break;
+  case 'development':
+    command = 'yarn build-dev:ssr';
+    break;
+  // case 'stage':
+  //   command = 'npm run build:dev';
+  //   break;
+  default:
+    command = 'yarn build:ssr'; // Default build command
+}
+
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Execution error: ${error}`);
+    return;
+  }
+  console.log(stdout);
+  console.error(stderr);
+});
