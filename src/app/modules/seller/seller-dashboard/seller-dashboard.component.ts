831c7d24950eac96bf9ec8154789f2bec8faab87
David Egbuwalo <lereegbuwalo@gmail.com>
2024-02-01 12:14:00 +0100

    fix: products units left on dashboard

--- src/app/modules/seller/seller-dashboard/seller-dashboard.component.ts
+++ src/app/modules/seller/seller-dashboard/seller-dashboard.component.ts
@@ -12,13 +12,21 @@ export class SellerDashboardComponent implements OnInit {
   loadingSummary: boolean;
   loading: boolean;
   dashboardData: any;
-  mostSelling = [];
+  mostSelling: {
+    productId: string;
+    name: string;
+    imageUrl: string;
+    unit: number;
+    category: string;
+    unitSold: number;
+    revenue: number;
+  }[] = [];
   user: any;
   userDetails: any;
 
   constructor(
     private dashboardService: DashboardService,
-    private userService: UserService
+    private userService: UserService,
   ) {}
 
   ngOnInit(): void {
