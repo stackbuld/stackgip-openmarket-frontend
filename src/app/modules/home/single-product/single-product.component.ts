b6bc51e9738f6d71ade8c5b87fe4cd084c62b181
David Egbuwalo <lereegbuwalo@gmail.com>
2024-03-28 13:12:27 +0100

    fix: product variants on checkout

--- src/app/modules/home/single-product/single-product.component.ts
+++ src/app/modules/home/single-product/single-product.component.ts
@@ -361,6 +361,7 @@ export class SingleProductComponent implements OnInit {
     this.shippingMethods = [this.defaultShipping];
     this.currentShippingMethod.next(this.defaultShipping);
     this.currentShippingMethod.subscribe();
+    this.sortedVariationsList = [];
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
     this.isTimeLate = this.getIfTimeLate();
@@ -434,6 +435,7 @@ export class SingleProductComponent implements OnInit {
     const productService$ = this.productService.getCachedProductById(
       this.productId,
     );
+    this.sortedVariationsList = [];
     productService$.subscribe({
       next: (res) => {
         this.isLoadingDetails = false;
