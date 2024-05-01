c4e581c5c95f94cee19b06b7b3c9cf67b99129db
demisfy <kelechi.amos.omeh@gmail.com>
2024-05-01 19:46:38 +0100

    feat: sort wallet transactions

--- src/app/shared/wallet/wallet-overview/wallet-overview.component.ts
+++ src/app/shared/wallet/wallet-overview/wallet-overview.component.ts
@@ -7,6 +7,7 @@ import {
 import { WalletService } from 'src/app/services/wallet/wallet.service';
 import { AuthService } from '../../../services/auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DateRange } from '../../components/date-range/date-range.dto';
 
 @Component({
   selector: 'app-wallet-overview',
@@ -22,18 +23,47 @@ export class WalletOverviewComponent implements OnInit {
   userId!: string;
   pageSize: number = 10;
   page: number = 1;
+  dateType: string = 'Sort by';
+  startDate: string = '';
+  endDate: string = '';
+
+  isDateRangeOpened: boolean = true;
+
+  transactionFilterValues: string[] = [
+    'all',
+    'month',
+    'week',
+    'today',
+    'custom',
+  ];
 
   constructor(
     private walletService: WalletService,
     private authService: AuthService,
     private router: Router,
-    private route: ActivatedRoute,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
     this.userId = this.authService.getLoggedInUser().id;
     this.getTransactions();
     this.getWithdrawalRequests();
+  }
+
+  applyDateRange(dateRange: DateRange): void {
+    this.startDate = dateRange.start;
+    this.endDate = dateRange.end;
+    this.getTransactions();
+    this.isDateRangeOpened = false;
+  }
+
+  applyFilter(): void {
+    if (this.dateType == 'custom') {
+      this.isDateRangeOpened = true;
+      return;
+    }
+    this.isDateRangeOpened = false;
+    this.getTransactions();
   }
 
   formatDate(date: Date) {
@@ -50,6 +80,9 @@ export class WalletOverviewComponent implements OnInit {
         userId: this.userId,
         pageSize: this.pageSize,
         page: this.page,
+        dateType: this.dateType,
+        startDate: this.startDate,
+        endDate: this.endDate,
       })
       .subscribe({
         next: (res) => {
